e5559592de4b2269c60f5878895b24753b321426
chgsnoopy <chgsnoopy@163.com>
2023-01-30 21:11:58 +0800

    Edit directly
    Edit directly for BOSS.JAVA

--- src/main/java/org/example/Boss.java
+++ src/main/java/org/example/Boss.java
@@ -3,7 +3,7 @@ package org.example;
 import java.io.Serializable;
 
 public class Boss implements Serializable {
-    //老师姓名
+    //老师姓名, edit directly
     private String BossName;
     public void setBossName(String name){
         BossName = name;
